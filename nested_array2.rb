85d05b38d338eabf00870873d93a53ee5381ad26
Roy Lee & John Olmsted <pair+lee-sh-roy.qsymmachus@devbootcamp.com>
2014-01-09 11:31:04 -0800

    Starts release 1; hash-array conversion

--- nested_array2.rb
+++ nested_array2.rb
@@ -3,6 +3,23 @@
 xo_generator = ["X", "X", "X", "X", "X", "O", "O", "O", "O", "O"].sample(9)
 board = Array.new(3) { xo_generator.shift(3) }
 
+#-----RELEASE 1-----
+
+data_table =[["Number",   "Name",        "Position",       "Points per Game"],
+             [ 12,        "Joe Schmo",   "Center",         [14, 32, 7, 0, 23]],
+             [ 9,         "Ms. Buckets", "Point Guard",    [19, 0, 11, 22, 0]],
+             [ 31,        "Harvey Kay",  "Shooting Guard", [0, 30, 16, 0, 25]],
+             [ 18,        "Sally Talls", "Power Forward",  [18, 29, 26, 31, 19]],
+             [ 22,        "MK DiBoux",   "Small Forward",  [11, 0, 23, 17, 0]]
+            ]
+
+roster = Hash.new
+i = 0
+while i <= 4
+  roster[i] = nil
+  i += 1
+end
+
 #-----SHITTIER VERSION-----
 
 # o_win = ["X", "X", "X", "X", "O", "O", "O", "O", "O"].shuffle!
@@ -18,3 +35,4 @@ board = Array.new(3) { xo_generator.shift(3) }
 
 #-----DRIVERS-----
 p board
+p roster
