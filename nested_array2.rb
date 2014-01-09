874c72f4b9d1e59e568c2aacea5cb6da7956b8e7
Roy Lee & John Olmsted <pair+lee-sh-roy.qsymmachus@devbootcamp.com>
2014-01-09 11:10:44 -0800

    Created tic tac toe board using sample; cleaner version

--- nested_array2.rb
+++ nested_array2.rb
@@ -1,12 +1,20 @@
-o_win = ["X", "X", "X", "X", "O", "O", "O", "O", "O"].shuffle!
-x_win = ["X", "X", "X", "X", "X", "O", "O", "O", "O"].shuffle!
-a = rand(2)
-board = Array.new(3) {
-  if a == 1
-    o_win.shift(3)
-  else
-    x_win.shift(3)
-  end
- }
-# p xo_generator
+#-----RELEASE 0-----
+
+xo_generator = ["X", "X", "X", "X", "X", "O", "O", "O", "O", "O"].sample(9)
+board = Array.new(3) { xo_generator.shift(3) }
+
+#-----SHITTIER VERSION-----
+
+# o_win = ["X", "X", "X", "X", "O", "O", "O", "O", "O"].shuffle!
+# x_win = ["X", "X", "X", "X", "X", "O", "O", "O", "O"].shuffle!
+# a = rand(2)
+# board = Array.new(3) {
+#   if a == 1
+#     o_win.shift(3)
+#   else
+#     x_win.shift(3)
+#   end
+#  }
+
+#-----DRIVERS-----
 p board
